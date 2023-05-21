84068d37115ad510e55d88b938345eb4db4c3ff5
Nivando <sidvandoni@gmail.com>
2023-05-21 01:13:30 -0300

    set switch theme button

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { useEffect, useState } from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <div>
+      <Component {...pageProps} />
+      <button className={`theme-toggle ${theme}`} onClick={toggleTheme}>
+        {theme === "light" ? <FiMoon size={24} /> : <FiSun size={24} />}
+      </button>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
