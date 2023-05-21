d8dc888af5d15ac43fcff718e84ea61a91928d06
Nivando <sidvandoni@gmail.com>
2023-05-21 19:47:00 -0300

    try build

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,9 +1,9 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import React from 'react'
-import Head from 'next/head'
-import ReactMarkdown from 'react-markdown'
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import React from "react";
+import Head from "next/head";
+import ReactMarkdown from "react-markdown";
 
 export default function Post({ content, data }) {
   return (
@@ -16,33 +16,33 @@ export default function Post({ content, data }) {
         <ReactMarkdown>{content}</ReactMarkdown>
       </article>
     </div>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const postsDirectory = path.join(process.cwd(), 'posts')
-  const filenames = fs.readdirSync(postsDirectory)
+  const postsDirectory = path.join(process.cwd(), "content");
+  const filenames = fs.readdirSync(postsDirectory);
 
   const paths = filenames.map((filename) => ({
-    params: { slug: filename.replace('.md', '') },
-  }))
+    params: { slug: filename.replace(".md", "") },
+  }));
 
   return {
     paths,
     fallback: false,
-  }
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const { slug } = params
-  const filePath = path.join(process.cwd(), 'posts', `${slug}.md`)
-  const fileContents = fs.readFileSync(filePath, 'utf8')
-  const { content, data } = matter(fileContents)
+  const { slug } = params;
+  const filePath = path.join(process.cwd(), "content", `${slug}.md`);
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  const { content, data } = matter(fileContents);
 
   return {
     props: {
       content,
       data,
     },
-  }
+  };
 }
